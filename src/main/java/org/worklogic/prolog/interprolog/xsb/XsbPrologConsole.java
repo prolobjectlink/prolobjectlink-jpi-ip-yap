7df5ccfd904f7d472381993bbe42b413deee4de7
Jose Zalacain <Jose Zalacain@169.254.166.73>
2019-01-31 19:23:27 -0400

    Console OK

--- src/main/java/org/worklogic/prolog/interprolog/xsb/XsbPrologConsole.java
+++ src/main/java/org/worklogic/prolog/interprolog/xsb/XsbPrologConsole.java
@@ -22,41 +22,14 @@ package org.worklogic.prolog.interprolog.xsb;
 import org.logicware.prolog.AbstractConsole;
 import org.logicware.prolog.PrologConsole;
 
-import com.declarativa.interprolog.PrologEngine;
-import com.declarativa.interprolog.XSBSubprocessEngine;
-import com.xsb.interprolog.NativeEngine;
-
 public class XsbPrologConsole extends AbstractConsole implements PrologConsole {
 
 	public XsbPrologConsole() {
-		super(null/* new XsbProlog() */);
+		super(new XsbProlog());
 	}
 
 	public static void main(String[] args) {
-//		new XsbPrologConsole().run(args);
-		subprocessEngine("C:/Program Files (x86)/XSB");
-		nativeEngine("/path/to/libxsb.so/parent/dir");
+		new XsbPrologConsole().run(args);
 	}
 
-	private static void subprocessEngine(String xsbPath) {
-		PrologEngine engine = new XSBSubprocessEngine(xsbPath);
-		if (engine.deterministicGoal(
-				"writeln('Hello XSB'), javaMessage('java.lang.System'-out,println(string('Hi Java, I am a sub-process!')))")) {
-			System.out.println("Subprocess engine is OK!");
-		} else {
-			System.out.println("Subprocess engine is NOT OK!");
-		}
-		engine.shutdown();
-	}
-
-	private static void nativeEngine(String xsbPath) {
-		PrologEngine engine = new NativeEngine(xsbPath);
-		if (engine.deterministicGoal(
-				"writeln('Hello XSB'), javaMessage('java.lang.System'-out,println(string('Hi Java, I am native!')))")) {
-			System.out.println("Native engine is OK!");
-		} else {
-			System.out.println("Native engine is NOT OK!");
-		}
-		engine.shutdown();
-	}
 }
