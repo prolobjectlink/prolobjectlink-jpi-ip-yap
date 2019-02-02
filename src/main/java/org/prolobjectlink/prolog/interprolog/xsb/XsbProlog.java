d33dbb6b56a06f1dc765b9a0676cf8755222f586
Jose Zalacain <Jose Zalacain@10.11.0.25>
2019-02-02 10:58:48 -0400

    Replace WorkLogic by Prolobjectlink

---
+++ src/main/java/org/prolobjectlink/prolog/interprolog/xsb/XsbProlog.java
@@ -0,0 +1,56 @@
+/*
+ * #%L
+ * prolobjectlink-jpi-ip-xsb
+ * %%
+ * Copyright (C) 2012 - 2019 WorkLogic Project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.prolobjectlink.prolog.interprolog.xsb;
+
+import org.prolobjectlink.prolog.PrologConverter;
+import org.prolobjectlink.prolog.PrologEngine;
+import org.prolobjectlink.prolog.PrologProvider;
+import org.prolobjectlink.prolog.interprolog.InterPrologProvider;
+
+import com.declarativa.interprolog.AbstractPrologEngine;
+import com.declarativa.interprolog.TermModel;
+import com.declarativa.interprolog.XSBSubprocessEngine;
+
+public class XsbProlog extends InterPrologProvider implements PrologProvider {
+
+	// path to binary engine directory
+	private static String xsbPath = "C:\\Program Files (x86)\\XSB\\bin";
+//		private static String xsbPath = "C:\\Program Files (x86)\\XSB\\config\\x64-pc-windows\\bin";
+
+	public XsbProlog() {
+		super(new XsbPrologConverter());
+	}
+
+	public XsbProlog(PrologConverter<TermModel> converter) {
+		super(converter);
+	}
+
+	public PrologEngine newEngine() {
+//			AbstractPrologEngine engine = new NativeEngine(xsbPath);
+		AbstractPrologEngine engine = new XSBSubprocessEngine(xsbPath);
+		return new XsbPrologEngine(this, engine);
+	}
+
+	@Override
+	public String toString() {
+		return "XsbProlog [converter=" + converter + "]";
+	}
+
+}
